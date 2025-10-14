88c9c57b2898e63381f644008eed5905977198e8
leonardo09082007 <163143692+leonardo09082007@users.noreply.github.com>
2025-10-14 10:13:21 -0300

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: "127.0.0.1",
     port: "3306",
     user: "root",
-    password: "escola", //senha "escola" no linux
+    password: "", //senha "escola" no linux
     database: "api_epa"
 })
 module.exports = Object.freeze({
