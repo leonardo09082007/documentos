184a2f99dc151d69ceb149579cae91de66184529
leonardo09082007 <163143692+leonardo09082007@users.noreply.github.com>
2025-10-28 12:09:44 -0300

    Add login endpoint and JWT authentication

--- index.js
+++ index.js
@@ -8,28 +8,62 @@ app.use(express.json())
 // npm i mysql2
 const db = require("./db")
 
-//npm i cors
+// npm i cors
 const cors = require("cors")
 app.use(cors())
 
-app.post("/cadastrar", async (req,res)=>{
+// npm i jsonwebtoken
+const jwt = require("jsonwebtoken")
+
+
+
+app.post("/cadastrar", async (req, res) =>{
     const cliente = req.body
-        try {
-const resultado = await db.pool.query(
-    `INSERT INTO cliente (nome,cpf,email,telefone,rua,n_casa,bairro,cidade,uf,cep,senha)
-    values (?,?,?,?,?,?,?,?,?,?,?)`,
-    [cliente.nome, cliente.cpf, cliente.email, cliente.telefone, cliente.rua, cliente.n_casa, cliente.bairro,
-        cliente.cidade, cliente.uf, cliente.cep, cliente.senha
-    ]
-)
-res.status(200).json({id: resultado[0]. insertId})
+    try {
 
-        } catch (erro) {
-            console.log(erro)
-            res.status(500).json({erro: "Erro interno na API"})
-        }
+        const resultado = await db.pool.query(
+            `INSERT INTO cliente (nome, cpf, email, telefone, rua, n_casa, bairro, cidade, uf, cep, senha)
+            values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [cliente.nome, cliente.cpf, cliente.email, cliente.telefone, cliente.rua, cliente.n_casa, 
+            cliente.bairro, cliente.cidade, cliente.uf, cliente.cep, cliente.senha])
+    res.status(200).json({id: resultado[0]. insertId})
 
+    } catch (erro) {
+        res.status(500).json({erro: "Erro interno na API"})
+        console.log(erro)
+    }
 })
+
+app.post("/login", async (req,res)=>{
+    const login = req.body;
+    if(login.email == null){
+        return res.status(400).json({erro: "Informe o email"})
+    }
+    if(login.senha == null){
+        return res.status(400).json({erro: "Informe a senha"})
+    }
+    try{
+        const resultado = await db.pool.query(
+            'SELECT*FROM cliente WHERE email = ?',
+            [login.email])
+
+            if(!resultado[0][0]){
+                return res.status(401).json({erro: "Email não cadastrado"})
+            }
+    
+            if(resultado[0][0].senha!=login.senha){
+                return res.status(401).json({erro: "Credenciais Inválidas"})
+            }
+
+    
+    }catch (erro) {
+        res.status(500).json({erro: "Erro interno na API"})
+        console.log(erro)
+    }
+    const token = jwt.sign(dados, "senha_muito_forte", {expiresIn: "1m"})
+    return res.status(200).json({token: token})
+})
+
 
 app.get("/clientes", async (req,res) => {
     try {
